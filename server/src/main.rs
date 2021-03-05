bb2dca7c8b896de9059bbeeb20f6664e56556001
dan sinclair <dj2@everburning.com>
2021-03-05 10:36:13 -0500

    Switch to Rocket.
    This CL switches from raw Hyper + Routerify + Handlebars to using
    Rocket. This provides better integration of the components and makes
    development easier.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,222 +1,98 @@
-use hyper::header::{self, HeaderValue};
-use hyper::{Body, Request, Response, Result, Server, StatusCode};
-use routerify::prelude::*;
-use routerify::{Middleware, RequestInfo, Router, RouterService};
-use std::env;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::path::{Path, PathBuf};
-use tokio::fs::File;
-use tokio::io::AsyncReadExt;
-use tokio_util::codec::{BytesCodec, FramedRead};
-use handlebars::Handlebars;
+mod archive;
+
+use rocket_contrib::templates::Template;
+use rocket::{Request, State};
+use rocket::http::ContentType;
+use rocket::response::{content, NamedFile};
+use std::path::PathBuf;
 use std::collections::HashMap;
+use rocket_contrib::serve::{crate_relative, StaticFiles};
+use archive::Archive;
+use tokio::sync::RwLock;
+use tokio::io::AsyncReadExt;
+use std::env;
 
 #[macro_use]
-extern crate lazy_static;
+extern crate rocket;
 
-static SERVER_DEFAULT_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
-static SERVER_DEFAULT_PORT: u16 = 3000;
+#[get("/asset/<file..>")]
+async fn assets(file: PathBuf, state: State<'_, ArchiveState>) -> Option<NamedFile> {
+    let archive = state.archive.read().await;
+    archive.retrieve_asset(file).await
+}
+
+#[derive(serde::Serialize)]
+struct NoteContext<'a> {
+    id: &'a str,
+    content: &'a str,
+    parent: &'static str,
+}
+#[get("/note/<name..>", rank = 2, format = "text/html")]
+async fn note_html(name: PathBuf, state: State<'_, ArchiveState>) -> Template {
+    let archive = state.archive.read().await;
+    let id = name.to_str().unwrap().to_string();
+
+    let mut file = archive.retrieve_note(name).await.unwrap();
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).await;
+
+    let ctx = NoteContext {
+        id: &id,
+        content: &buf,
+        parent: "layout",
+    };
+    Template::render("show", &ctx)
+}
+
+#[get("/note/<name..>", rank = 1, format = "text/plain")]
+async fn note_plain(name: PathBuf, state: State<'_, ArchiveState>) -> content::Content<NamedFile> {
+    let archive = state.archive.read().await;
+    content::Content(ContentType::Plain,
+                    archive.retrieve_note(name).await.unwrap())
+}
+
+#[derive(serde::Serialize)]
+struct IndexContext {
+    parent: &'static str,
+}
+#[get("/")]
+async fn index<'a>() -> Template {
+    let ctx = IndexContext {
+        parent: "layout",
+    };
+    Template::render("index", &ctx)
+}
+
+#[catch(404)]
+fn not_found(req: &Request<'_>) -> Template {
+    let mut map = HashMap::new();
+    map.insert("path", req.uri().path());
+
+    Template::render("404", &map)
+}
+
+struct ArchiveState {
+    archive: RwLock<Archive>,
+}
+
 static SERVER_DEFAULT_ASSET_PATH: &str = "./data/assets";
 static SERVER_DEFAULT_DATA_PATH: &str = "./data/data";
 
-static NOTFOUND: &str = "Not Found";
-
-lazy_static! {
-  static ref HB: Handlebars<'static> = {
-    let mut hb = Handlebars::new();
-    if let Err(e) = hb.register_template_file("index", "./server/views/index.hbs") {
-      panic!("Failed to load template: index\n{}", e);
-    }
-    if let Err(e) = hb.register_template_file("layout", "./server/views/layout.hbs") {
-      panic!("Failed to load template: layout\n{}", e);
-    }
-    hb
-  };
-}
-
-struct State<'a> {
-    asset_path: String,
-    data_path: String,
-    hb: &'a Handlebars<'a>,
-}
-
-#[tokio::main]
-async fn main() {
-    let addr = SocketAddr::new(
-        env::var("ARCHIVE_HOST")
-            .ok()
-            .and_then(|host| host.parse::<IpAddr>().ok())
-            .unwrap_or(SERVER_DEFAULT_IP),
-        env::var("ARCHIVE_PORT")
-            .ok()
-            .and_then(|port| port.parse::<u16>().ok())
-            .unwrap_or(SERVER_DEFAULT_PORT),
-    );
+#[launch]
+fn rocket() -> rocket::Rocket {
+    let rocket = rocket::ignite()
+        .attach(Template::fairing())
+        .register(catchers![not_found])
+        .mount("/", StaticFiles::from(crate_relative!("public")))
+        .mount("/", routes![assets, index])
+        .mount("/", routes![note_plain, note_html]);
 
     let asset_path =
         env::var("ARCHIVE_ASSET_PATH").unwrap_or_else(|_| SERVER_DEFAULT_ASSET_PATH.to_string());
     let data_path =
         env::var("ARCHIVE_DATA_PATH").unwrap_or_else(|_| SERVER_DEFAULT_DATA_PATH.to_string());
 
-    let router = router(asset_path, data_path);
-    let service = RouterService::new(router).unwrap();
+    let archive = Archive::new(&data_path, &asset_path);
 
-    let server = Server::bind(&addr).serve(service);
-    let server = server.with_graceful_shutdown(shutdown());
-
-    println!("Listening on http://{}", addr);
-
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
-    }
-}
-
-async fn shutdown() {
-    tokio::signal::ctrl_c()
-        .await
-        .expect("failed to install ctrl-c signal handler");
-}
-
-async fn server_middleware(mut res: Response<Body>) -> Result<Response<Body>> {
-    res.headers_mut()
-        .insert(header::SERVER, HeaderValue::from_str("Archive").unwrap());
-
-    if !res.headers().contains_key(header::CONTENT_TYPE) {
-        res.headers_mut().insert(
-            header::CONTENT_TYPE,
-            HeaderValue::from_str("text/html").unwrap(),
-        );
-    }
-
-    Ok(res)
-}
-
-async fn logger_middleware(req: Request<Body>) -> Result<Request<Body>> {
-    println!(
-        "{} {} {}",
-        req.remote_addr(),
-        req.method(),
-        req.uri().path()
-    );
-    Ok(req)
-}
-
-async fn error_handler(err: routerify::Error, _: RequestInfo) -> Response<Body> {
-    eprintln!("{}", err);
-
-    Response::builder()
-        .status(StatusCode::INTERNAL_SERVER_ERROR)
-        .body(Body::from(format!("Something went wrong: {}", err)))
-        .unwrap()
-}
-
-async fn index_handler(req: Request<Body>) -> Result<Response<Body>> {
-    let state = req.data::<State>().unwrap();
-
-    let mut d = HashMap::new();
-    d.insert("parent", "layout");
-
-    let body = state.hb.render("index", &d).ok();
-    if let Some(body) = body {
-      Ok(Response::new(Body::from(body)))
-    } else {
-      Ok(not_found())
-    }
-}
-
-async fn static_handler(req: Request<Body>) -> Result<Response<Body>> {
-    let path = clean_path("server/public", &req.uri().path());
-    if let Some(path) = path {
-        return send_file(&path).await;
-    }
-    Ok(not_found())
-}
-
-async fn asset_handler(req: Request<Body>) -> Result<Response<Body>> {
-    let state = req.data::<State>().unwrap();
-    let path = clean_path(&state.asset_path, &req.uri().path()[7..]);
-    if let Some(path) = path {
-        return send_file(&path).await;
-    }
-    Ok(not_found())
-}
-
-async fn note_handler(req: Request<Body>) -> Result<Response<Body>> {
-    let state = req.data::<State>().unwrap();
-    let path = clean_path(&state.data_path, &req.uri().path()[6..]);
-    if let Some(path) = path {
-        if let Ok(mut file) = File::open(path).await {
-            let mut body = Vec::new();
-            if file.read_to_end(&mut body).await.is_ok() {
-                let body = String::from_utf8_lossy(&body).into_owned();
-                let mut resp = Response::new(Body::from(body));
-                resp.headers_mut().insert(
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_str("text/html").unwrap(),
-                );
-                return Ok(resp);
-            }
-        }
-    }
-    Ok(not_found())
-}
-
-async fn send_file(path: &Path) -> Result<Response<Body>> {
-    if let Ok(file) = File::open(path).await {
-        let ty = tree_magic::from_filepath(path);
-        let stream = FramedRead::new(file, BytesCodec::new());
-        let body = Body::wrap_stream(stream);
-
-        let mut resp = Response::new(body);
-        resp.headers_mut()
-            .insert(header::CONTENT_TYPE, HeaderValue::from_str(&ty).unwrap());
-        return Ok(resp);
-    }
-
-    Ok(not_found())
-}
-
-fn router(asset_path: String, data_path: String) -> Router<Body, hyper::Error> {
-  let state = State {
-        asset_path,
-        data_path,
-        hb: &HB,
-    };
-
-    Router::builder()
-        .data(state)
-        .middleware(Middleware::pre(logger_middleware))
-        .middleware(Middleware::post(server_middleware))
-        .get("/css/*", static_handler)
-        .get("/js/*", static_handler)
-        .get("/notes/:name", note_handler)
-        .get("/assets/*", asset_handler)
-        .get("/index.html", index_handler)
-        .get("/", index_handler)
-        .err_handler_with_info(error_handler)
-        .build()
-        .unwrap()
-}
-
-fn clean_path(prefix: &str, path: &str) -> Option<PathBuf> {
-    let mut s = path.to_string();
-    if !s.starts_with('/') {
-        s = format!("/{}", s);
-    }
-
-    let mut path = PathBuf::from(prefix);
-    for p in s.split('/') {
-        if p == ".." || p == "." {
-            continue;
-        }
-        path.push(p);
-    }
-    path.canonicalize().ok()
-}
-
-fn not_found() -> Response<Body> {
-    Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(NOTFOUND.into())
-        .unwrap()
+    rocket.manage(ArchiveState { archive: RwLock::new(archive) })
 }
