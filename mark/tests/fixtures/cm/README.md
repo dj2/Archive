4df9fc3206c412b588ce30b28c437ca3bb3b5933
dan sinclair <dj2@everburning.com>
2021-03-08 22:49:50 -0500

    [mark] Generate commonmark test files.
    This creates the common mark test files from the 0.29 spec. The
    currently failing tests are marked as ignored. Some of these tests will
    not be made to pass, and will be commented as such where they deviate.

---
+++ mark/tests/fixtures/cm/README.md
@@ -0,0 +1,6 @@
+
+# CommonMark Files.
+
+The files in tests/fixtures/cm are generated from
+https://spec.commonmark.org/0.29/spec.json and are licensed under
+<https://creativecommons.org/licenses/by-sa/4.0/>.
--- mark/tests/fixtures/mod.rs
+++ mark/tests/fixtures/mod.rs
@@ -3,9 +3,9 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-fn compare(name: &str) {
-    let html = format!("tests/fixtures/data/{}.html", name);
-    let md = format!("tests/fixtures/data/{}.md", name);
+pub fn compare(name: &str) {
+    let html = format!("tests/fixtures/{}.html", name);
+    let md = format!("tests/fixtures/{}.md", name);
 
     let mut result = String::new();
     File::open(Path::new(&html))
@@ -25,47 +25,49 @@ fn compare(name: &str) {
     assert_eq!(result, actual.trim_end());
 }
 
+mod cm;
+
 #[test]
 pub fn blockquote() {
-    compare("blockquote")
+    compare("data/blockquote")
 }
 
 #[test]
 pub fn paragraphs() {
-    compare("paragraphs")
+    compare("data/paragraphs")
 }
 
 #[test]
 pub fn headers() {
-    compare("headers")
+    compare("data/headers")
 }
 
 #[test]
 pub fn thematic_breaks() {
-    compare("thematic_breaks")
+    compare("data/thematic_breaks")
 }
 
 #[test]
 pub fn fenced_code() {
-    compare("fenced_code")
+    compare("data/fenced_code")
 }
 
 #[test]
 pub fn list() {
-    compare("list")
+    compare("data/list")
 }
 
 #[test]
 pub fn em() {
-    compare("em")
+    compare("data/em")
 }
 
 #[test]
 pub fn strong() {
-    compare("strong")
+    compare("data/strong")
 }
 
 #[test]
 pub fn code() {
-    compare("code")
+    compare("data/code")
 }
